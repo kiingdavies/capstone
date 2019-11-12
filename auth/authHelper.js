2a6b09b2bf20121a2914fcb5d37787f38134a344
kiingdavies <daviesadedeji007@gmail.com>
2019-11-12 22:50:30 +0100

    first commit

--- auth/authHelper.js
+++ auth/authHelper.js
@@ -12,7 +12,7 @@ const Helper = {
         return bcrypt.compare(password, hashPassword);
     },
     /**
-     * isValidEmail helper method
+     * isValidEmail helper method 
      * @param {string} email
      * @returns {Boolean} True or False
      */
