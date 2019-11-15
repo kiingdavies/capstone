09199cd1481793db52175b3b27805e48ac94c525
kiingdavies <50304558+kiingdavies@users.noreply.github.com>
2019-11-15 07:57:44 +0100

    Update feed.js

--- api/routes/feed.js
+++ api/routes/feed.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 const pool = new pg.Pool({
     port: 5432,
-    password: 'adedeji007',
+    password: '',
     database: 'teamwork',
     max: 10,
     host: 'localhost',
